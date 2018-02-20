84960225220b0abd8e13d5015aeb40546c2e0f42
badr sk1x <badrsk1x@gmail.com>
2018-02-20 13:40:20 +0300

    Config Vars

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('AuthLogin', AUTH_LOGIN) ;
+define('AuthPassword', AUTH_PASSWORD) ;
+define('TariffId', '1');
+define('StandartWeight', '0.1');
+define('StandartVolume', '0.1');
+define('CalculatorURL','http://api.cdek.ru/calculator/calculate_price_by_json.php');
