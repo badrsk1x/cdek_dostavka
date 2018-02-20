041b3f1e9daf84d830b8c0474a7fa84d250746fa
Badr CH <badrsk1x@gmail.com>
2018-02-20 20:08:30 +0300

    Update pages_controller.php

--- Controllers/pages_controller.php
+++ Controllers/pages_controller.php
@@ -1,17 +1,10 @@
 <?php
 namespace Controllers\PagesController;
 
-//use Vendor\Transport\Transport ;
-//use Controllers\RealEstateController\RealEstateController;
-
 class PagesController
 {
     public function home()
     {
-      //  $obj = new Transport();
-      //  $metros = $obj->GetMetroFromFile(METRO_FILE);
-      //  $requests = new RealEstateController($_POST);
-
         require_once('Views/form_search.php');
     }
 
